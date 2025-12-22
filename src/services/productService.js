c104e4b0f507b65d25b4c0fd490284b59b75dfc0
N1coG4bo <nic.lagosf@duocuc.cl>
2025-12-22 13:58:37 -0300

    Se agrega conexión a backend MongoDB

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,4 +1,4 @@
-import products from '../data/products.json';
+/* import products from '../data/products.json';
 
 // Obtener todos los productos (simula una espera de 0.5 seg)
 export const getProducts = () => {
@@ -17,4 +17,41 @@ export const getProductByCode = (code) => {
       resolve(product);
     }, 500);
   });
+};
+
+*/
+
+
+// Definimos la URL de tu backend en la nube (Render)
+// Nota: Le agregamos 'https://' al principio y '/api' al final
+const API_URL = 'https://backend-plantilla-comercio-online.onrender.com/api';
+
+// Obtener todos los productos (Petición REAL al servidor)
+export const getProducts = async () => {
+  try {
+    const response = await fetch(`${API_URL}/products`);
+    
+    if (!response.ok) {
+      throw new Error('No se pudo conectar con el servidor');
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error trayendo productos del Backend:", error);
+    return []; // Retorna arreglo vacío para evitar que la app explote
+  }
+};
+
+// Obtener un producto por su código
+export const getProductByCode = async (code) => {
+  try {
+    // Estrategia: Reutilizamos getProducts para asegurar que tenemos la lista actualizada
+    // y buscamos el producto específico aquí en el cliente.
+    const products = await getProducts();
+    return products.find(p => p.code === code);
+  } catch (error) {
+    console.error("Error buscando producto:", error);
+    return null;
+  }
 };
